942cc9585bd74fe5bada842f7a6202045cd172c0
giligran <kuprykovrodion@gmail.com>
2023-09-24 11:45:44 +0300

    connect mongosse

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// mongodb+srv://kuprykovrodion:epp8njC9ksbhPbk@phonebook.14yk2yv.mongodb.net/dab-contacts?retryWrites=true&w=majority&appName=AtlasApp
+
+const DB_HOST =
+  "mongodb+srv://kuprykovrodion:epp8njC9ksbhPbk@phonebook.14yk2yv.mongodb.net/db-contacts?retryWrites=true&w=majority&appName=AtlasApp";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Connect success");
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
