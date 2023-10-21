509552c055f8769c0f678be43920cdd06261484a
giligran <kuprykovrodion@gmail.com>
2023-10-21 15:27:25 +0300

    publish branch and update models, routes, controlers

---
+++ models/users.js
@@ -0,0 +1,62 @@
+const { Schema, model } = require("mongoose");
+const { handleMongooseError } = require("../helpers");
+const Joi = require("joi");
+
+const emailRegexp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,8})+$/;
+const subscriptionList = ["starter", "pro", "business"];
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      match: emailRegexp,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      minlength: 6,
+      required: true,
+    },
+    subscription: {
+      type: String,
+      enum: subscriptionList,
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: "",
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleMongooseError);
+
+const registerSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const updateSubscriptionSchema = Joi.object({
+  subscription: Joi.string()
+    .valid(...subscriptionList)
+    .required(),
+});
+
+const schemas = {
+  registerSchema,
+  loginSchema,
+  updateSubscriptionSchema,
+};
+const User = model("user", userSchema);
+
+module.exports = {
+  User,
+  schemas,
+};
