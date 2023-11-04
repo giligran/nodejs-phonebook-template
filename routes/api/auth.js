884e2a45ab67907ff0ab74e7a588c0c926baeb5d
giligran <kuprykovrodion@gmail.com>
2023-11-04 15:54:47 +0200

    hw05

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { validateBody, authenticate } = require("../../middlewares");
+const { validateBody, authenticate, upload } = require("../../middlewares");
 const { schemas } = require("../../models/user");
 const ctrl = require("../../controllers/auth");
 
@@ -14,6 +14,12 @@ router.patch(
   authenticate,
   validateBody(schemas.updateSubscriptionSchema),
   ctrl.updateSubscription
+);
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrl.updateAvatar
 );
 
 module.exports = router;
